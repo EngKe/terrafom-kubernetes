557617a359ac1660f0653ee768d96bc208707c99
EngKe <48016002+EngKe@users.noreply.github.com>
2022-10-29 02:13:24 +0300

    Add files via upload

--- database/connect-outside-k8s.sh
+++ database/connect-outside-k8s.sh
@@ -1,16 +1,1 @@
-port_forward(){
-
-until  kubectl -n default get pod -o jsonpath='{range .items[*]}{.status.containerStatuses[*].ready.true}{.metadata.name}{ "\n"}{end}' | grep redis-replicas-2
-    do 
-    echo 'pod not running yet'
-    sleep 10
-    done
-echo 'port fowarding'
-sleep 5
-sudo systemctl stop redis-server.service
-kubectl port-forward --namespace default svc/postgresql 5432:5432 &
-kubectl port-forward --namespace default svc/redis-master 6379:6379 &
-
-}
-
-port_forward
+sudo systemctl enable port-forward.service
