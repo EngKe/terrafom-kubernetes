f59d7e9dab91605992401ec5fdb4ffa310a90b7a
EngKe <48016002+EngKe@users.noreply.github.com>
2022-10-30 00:06:38 +0300

    Update database-infrastructures.sh

--- database/database-infrastructures.sh
+++ database/database-infrastructures.sh
@@ -4,7 +4,7 @@ postgresql(){
 
     mkdir -p backup
     sudo chown vagrant backup
-    (crontab -l 2>/dev/null; echo "*/5 * * * * /home/vagrant/backup.sh") | crontab -
+    (crontab -l 2>/dev/null; echo "0 1 * * * /home/vagrant/backup.sh") | crontab -
 
     chmod 750 postgresql-test.sh
     chmod 750 redis-test.sh
